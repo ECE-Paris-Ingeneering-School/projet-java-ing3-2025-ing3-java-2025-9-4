313e7f0468bcd5a596baa04b4a29ada5a9745a94
thomasgit1234 <thomas.chareyron@edu.ece.fr>
2025-04-19 00:27:06 +0200

    Create Disponibilite.java

---
+++ src/modele/Disponibilite.java
@@ -0,0 +1,34 @@
+package modele;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class Disponibilite {
+    private int id;
+    private Specialiste specialiste;
+    private Lieu lieu;
+    private Date date;
+    private Time heure;
+    private boolean disponible;
+
+    public Disponibilite(int id, Specialiste specialiste, Lieu lieu, Date date, Time heure, boolean disponible) {
+        this.id = id;
+        this.specialiste = specialiste;
+        this.lieu = lieu;
+        this.date = date;
+        this.heure = heure;
+        this.disponible = disponible;
+    }
+
+    public int getId() { return id; }
+    public Specialiste getSpecialiste() { return specialiste; }
+    public Lieu getLieu() { return lieu; }
+    public Date getDate() { return date; }
+    public Time getHeure() { return heure; }
+    public boolean isDisponible() { return disponible; }
+
+    @Override
+    public String toString() {
+        return date + " à " + heure + " avec " + specialiste + " à " + lieu;
+    }
+}
