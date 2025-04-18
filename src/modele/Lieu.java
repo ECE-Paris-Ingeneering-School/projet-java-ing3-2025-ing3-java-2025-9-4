2036dc968322f164e6996dfb855501be7cfcf32f
Gabriel D <gabriel.dalibert@gmail.com>
2025-04-18 23:53:44 +0200

    Ajout Model Lieu

---
+++ src/modele/Lieu.java
@@ -0,0 +1,22 @@
+package modele;
+
+public class Lieu {
+    private int id;
+    private String adresse;
+    private String ville;
+
+    public Lieu(int id, String adresse, String ville) {
+        this.id = id;
+        this.adresse = adresse;
+        this.ville = ville;
+    }
+
+    public int getId() { return id; }
+    public String getAdresse() { return adresse; }
+    public String getVille() { return ville; }
+
+    @Override
+    public String toString() {
+        return ville + " - " + adresse;
+    }
+}
