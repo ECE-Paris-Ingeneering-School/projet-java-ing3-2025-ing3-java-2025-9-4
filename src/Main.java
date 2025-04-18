e2200b117d6ada566a9d4f616728f51b02460db3
Gabriel D <gabriel.dalibert@gmail.com>
2025-04-18 23:56:56 +0200

    Ajout Modif Main

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import controleur.AuthentificationController;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-
-        System.out.printf("Test push Malo");
-
-        System.out.printf("Test push Thomas");
-
-        System.out.printf("Test push Gabriel");
-
-        System.out.printf("Test push Fethi");
-
-
-
+        new AuthentificationController();
     }
 }
