2d67903e5877263c450025564dd45ea2d912b100
Gabriel D <gabriel.dalibert@gmail.com>
2025-04-19 23:51:56 +0200

    Ajout de commentaire pour Lieu Model

--- src/modele/Lieu.java
+++ src/modele/Lieu.java
@@ -2,22 +2,22 @@ package modele;
 
 
 public class Lieu {
-    private int id;
+    private int id; // Initialisation variables
     private String adresse;
     private String ville;
 
-    public Lieu(int id, String adresse, String ville) {
+    public Lieu(int id, String adresse, String ville) { // Création du constructeur lieu
         this.id = id;
         this.adresse = adresse;
         this.ville = ville;
     }
 
-    public int getId() { return id; }
-    public String getAdresse() { return adresse; }
-    public String getVille() { return ville; }
+    public int getId() { return id; } // Renvoie l'ID
+    public String getAdresse() { return adresse; } // Renvoie l'adresse
+    public String getVille() { return ville; } // renvoie la ville
 
     @Override
     public String toString() {
         return ville + " - " + adresse;
-    }
+    } // Renvoie la ville et l'adresse
 }
