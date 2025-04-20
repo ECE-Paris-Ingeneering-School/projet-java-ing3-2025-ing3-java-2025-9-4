cb573941ee89babcb26e11f480c50e4ab8bee6da
thomasgit1234 <thomas.chareyron@edu.ece.fr>
2025-04-20 18:14:12 +0200

    Update AgendaController.java

--- src/controleur/AgendaController.java
+++ src/controleur/AgendaController.java
@@ -1,5 +1,6 @@
 package controleur;
 
+// Importation des classes nécessaires
 import dao.RendezVousDAO;
 import modele.RendezVous;
 import vue.AgendaPatientVue;
@@ -7,16 +8,22 @@ import vue.AgendaPatientVue;
 import java.util.List;
 
 public class AgendaController {
-    private AgendaPatientVue vue;
-    private RendezVousDAO dao;
+    // Déclaration des attributs principaux
+    private AgendaPatientVue vue;     // Vue affichant l’agenda du patient
+    private RendezVousDAO dao;        // DAO permettant d’accéder aux rendez-vous
 
+    // Constructeur du contrôleur
     public AgendaController(int idPatient, String nomPatient) {
-        dao = new RendezVousDAO();
-        vue = new AgendaPatientVue(nomPatient);
+        dao = new RendezVousDAO();                    // Initialisation du DAO pour les rendez-vous
+        vue = new AgendaPatientVue(nomPatient);       // Création de la vue avec le nom du patient
 
+        // Récupération de la liste des rendez-vous pour le patient donné
         List<RendezVous> rdvList = dao.getRendezVousParPatient(idPatient);
+
+        // Affichage de la liste des rendez-vous dans la vue
         vue.afficherListeRdv(rdvList);
 
+        // Rendre la vue visible
         vue.setVisible(true);
     }
 }
