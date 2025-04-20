32061a70e5cc822865494e01cf711f3670d260ac
Gabriel D <gabriel.dalibert@gmail.com>
2025-04-20 18:57:53 +0200

    COmmentaire Main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import controleur.AuthentificationController;
 
 public class Main {
     public static void main(String[] args) {
-        new AuthentificationController();
+        new AuthentificationController(); // Lance la page d'authentification qui nous emmènera sur les autres pagse après
     }
 }
-
