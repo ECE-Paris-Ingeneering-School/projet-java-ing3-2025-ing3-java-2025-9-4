cb3aed6d14ba32f6df73b52478cd6b249882881e
Malo Colin <130997449+fiskin09@users.noreply.github.com>
2025-04-20 11:32:27 +0200

    Update AdminDashboardController.java
    explication du code

--- src/controleur/AdminDashboardController.java
+++ src/controleur/AdminDashboardController.java
@@ -1,31 +1,37 @@
-package controleur;
+package controleur; //appartient au package
 
-import vue.AdminDashboardVue;
+import vue.AdminDashboardVue; //importation du document d un autre package pour pouvoir le lier
 
 public class AdminDashboardController {
+
     private AdminDashboardVue vue;
 
+    //Constructeur
     public AdminDashboardController() {
-        vue = new AdminDashboardVue();
+        vue = new AdminDashboardVue(); //création de la vue admin
 
+        //si clique sur bouton specialiste -> ouverture controleur correspondant
         vue.setSpecialistesListener(e -> new GestionSpecialistesController());
         {
             System.out.println("Gestion des spécialistes (à venir)");
             // new GestionSpecialistesController();
         };
 
+        //si clique sur bouton disponibilite -> ouverture controleur correspondant
         vue.setDisponibilitesListener(e -> new GestionDisponibilitesController());
         {
             System.out.println("Gestion des créneaux (à venir)");
             // new GestionDisponibilitesController();
         };
 
+        //si clique sur bouton patiens -> ouverture controleur correspondant
         vue.setPatientsListener(e -> new GestionPatientsController());
         {
             System.out.println("Gestion des patients (à venir)");
             // new GestionPatientsController();
         };
 
+        //si clique sur bouton deconnexion -> ouverture controleur correspondant
         vue.setDeconnexionListener(e -> {
             vue.dispose();
             new AuthentificationController();
