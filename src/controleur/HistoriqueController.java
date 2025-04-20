2182a6a66d5942b30a99219e7059282bdcbb0a3d
thomasgit1234 <thomas.chareyron@edu.ece.fr>
2025-04-20 18:14:58 +0200

    Update HistoriqueController.java

--- src/controleur/HistoriqueController.java
+++ src/controleur/HistoriqueController.java
@@ -1,5 +1,6 @@
 package controleur;
 
+// Importation des classes nécessaires
 import dao.RendezVousDAO;
 import modele.RendezVous;
 import vue.HistoriquePatientVue;
@@ -7,16 +8,23 @@ import vue.HistoriquePatientVue;
 import java.util.List;
 
 public class HistoriqueController {
-    private HistoriquePatientVue vue;
-    private RendezVousDAO dao;
+    // Attributs du contrôleur
+    private HistoriquePatientVue vue;    // Vue pour afficher l’historique des rendez-vous
+    private RendezVousDAO dao;           // DAO pour accéder aux données de rendez-vous
 
+    // Constructeur du contrôleur
     public HistoriqueController(int idPatient, String nomPatient) {
-        dao = new RendezVousDAO();
-        vue = new HistoriquePatientVue(nomPatient);
+        dao = new RendezVousDAO();                            // Instanciation du DAO
+        vue = new HistoriquePatientVue(nomPatient);           // Création de la vue avec le nom du patient
 
+        // Récupération de l'historique des rendez-vous pour ce patient
         List<RendezVous> historiques = dao.getHistoriqueParPatient(idPatient);
+
+        // Affichage de l'historique dans la vue
         vue.afficherHistorique(historiques);
 
+        // Affichage de la fenêtre
         vue.setVisible(true);
     }
 }
+
